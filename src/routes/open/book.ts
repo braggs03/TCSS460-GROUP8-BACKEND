77fb198074b0798fa36572ae7f40c6b2105ee399
Quienten <quientenmiller2@gmail.com>
2024-10-22 13:27:36 -0700

    Document /book/ to include optional params and default values

--- src/routes/open/book.ts
+++ src/routes/open/book.ts
@@ -2,9 +2,50 @@ import express, { Router } from "express";
 
 const bookRouter: Router = express.Router();
 
+/**
+ * @api {get} /book Request to get book(s) by title.
+ * @apiName GetBookByTitle
+ * @apiGroup Book
+ *
+ * @apiBody {String} [isbn] a book ISBN.
+ * @apiBody {String} [title] a book title.
+ * @apiBody {String} [author] a book author.
+ * @apiBody {String} [rating_min=0] a book rating.
+ * @apiBody {String} [rating_max=5] a book rating.
+ * @apiBody {String} [year_min=1900] a book year.
+ * @apiBody {String} [year_max=3000] a book year.
+ *
+ * @apiSuccess {Book} an object containing book information.
+ *
+ * @apiError (400: Missing Parameters) {String} message "Missing required information"
+ * @apiError (400: Malformed Authorization Header) {String} message "Malformed Authorization Header"
+ * @apiError (404: User Not Found) {String} message "User not found"
+ * @apiError (400: Invalid Credentials) {String} message "Credentials did not match"
+ *
+ */
 bookRouter.get("/", (request, response) => {
-    //request.params.title;
-    response.send("Hello, World!");
+    response.send(request.query.title);
+    //response.send("Hello, World!");
+
+    
 });
 
 export { bookRouter };
+
+
+/**
+ * /book?title=The%20Great%20Gatsby&rating=5
+ * /book/title?title=The%20Great%20Gatsby and /book/rating?rate=5
+ * 
+ * 
+SELECT * WHERE (
+        title = title AND 
+        rating > rate
+    )
+
+    //title
+    "title = title"
+    //rating
+    "RATING > rate" 
+
+*/
