901a31307747f41434d3f8a99782e9eb0859dbdc
Brandon <brandonragghianti@gmail.com>
2024-10-31 14:07:21 -0700

    Added documentation to rating and delete routes.

--- src/routes/open/book.ts
+++ src/routes/open/book.ts
@@ -56,7 +56,7 @@ bookRouter.get('/', (request, response) => {
 });
 
 /**
- * @api {get} /book/isbn Request to a book by ISBN.
+ * @api {get} /book/isbn Request to get a book by ISBN.
  * @apiName GetBookByISBN
  * @apiGroup Book
  *
@@ -65,7 +65,7 @@ bookRouter.get('/', (request, response) => {
  * @apiSuccess {Book} an object containing book information.
  * What if no books are found?
  * @apiError (400: Missing ISBN) {String} message "Missing 'isbn' query paremeter."
- * @apiError (400: Bad ISBN) {String} message "ISBN not valid. ISBN should be a positive 13 or 10 digit number."
+ * @apiError (400: Bad ISBN) {String} message "ISBN not valid. ISBN should be a positive 13 digit number."
  * @apiError (403: Invalid JWT) {String} message "Provided JWT is invalid. Please sign-in again."
  * @apiError (401: Authorization Token is not supplied) {String} message "No JWT provided, please sign in."
  *
@@ -165,51 +165,102 @@ bookRouter.get('/:author', (request, response) => {
 });
 
 /**
- * @api {get} /book/title Request to books withing given range.
+ * @api {get} /book/rating Request to books within a given range. If book(s) from a point and up are desired, only set rating_min. If book(s) from a point and below are desired, only set rating_max. Both can be suppiled to get the book(s) within that range inclusively. If a specific rating is required, set rating_min and rating_max to the same value.
  * @apiName GetBookByRating
  * @apiGroup Book
  *
- * @apiBody {number} [rating_min=0] a book rating.
- * @apiBody {number} [rating_max=5] a book rating.
+ * @apiBody {number} [rating_min=1] a minimum rating required for a book.
+ * @apiBody {number} [rating_max=5] a maximum rating required for a book.
  *
- * @apiSuccess {Book(s)} an object containing information of books within range of ratings.
+ * @apiSuccess {Book(s)} an object containing information of books within the range of ratings.
  *
- * @apiError (400: Missing Parameters) {String} message "Missing required information"
+ * @apiError (400: Missing max and min rating) {String} message "Missing max and min rating, atleast one of which should be supplied"
+ * @apiError (400: Bad Min Rating) {String} message "Min rating is not a valid rating, should be a floating point from 1 to 5."
+ * @apiError (400: Bad Max Rating) {String} message "Max rating is not a valid rating, should be a floating point from 1 to 5."
+ * @apiError (400: Min Rating Greater Than Max Rating) {String} message "The provided minimum rating is greater than the maximum rating."
+ * @apiError (403: Invalid JWT) {String} message "Provided JWT is invalid. Please sign-in again."
+ * @apiError (401: Authorization Token is not supplied) {String} message "No JWT provided, please sign in."
+ */
+bookRouter.get('/rating', (request, response) => {
+
+});
+
+/**
+ * @api {get} /book/isbn Request to get a book by ISBN.
+ * @apiName GetBookByISBN
+ * @apiGroup Book
+ *
+ * @apiBody {number} isbn a book ISBN.
+ *
+ * @apiSuccess {Book} an object containing book information.
+ * What if no books are found?
+ * @apiError (400: Missing ISBN) {String} message "Missing 'isbn' query paremeter."
+ * @apiError (400: Bad ISBN) {String} message "ISBN not valid. ISBN should be a positive 13 digit number."
  * @apiError (403: Invalid JWT) {String} message "Provided JWT is invalid. Please sign-in again."
  * @apiError (401: Authorization Token is not supplied) {String} message "No JWT provided, please sign in."
  *
  */
-bookRouter.get('/rating', (request, response) => {});
 
-/**
- * @api {delete} /book Request to delete book(s).
- * @apiName DeleteBookByAttributes
- * @apiGroup Book
- *
- * @apiBody {String} [isbn] a book ISBN.
- * @apiBody {String} [title] a book title.
- * @apiBody {String} [author] a book author.
- * @apiBody {String} [rating_min=0] a book rating.
- * @apiBody {String} [rating_max=5] a book rating.
- * @apiBody {String} [year_min=1600] a book year.
- * @apiBody {String} [year_max=3000] a book year.
- *
- * @apiSuccess {Book} an object containing book information.
- *
- * @apiError (400: Malformed Authorization Header) {String} message "Malformed Authorization Header"
- * @apiError (404: User Not Found) {String} message "User not found"
- * @apiError (400: Invalid Credentials) {String} message "Credentials did not match"
- * @apiError (400: ISBN Parameter Invalid) {String} message "ISBN parameter is invalid. An ISBN should be a positive 13 digit number."
- * @apiError (400: Title Parameter Invalid) {String} message "Title parameter is invalid. A title should be a non-empty string."
- * @apiError (400: Author Parameter Invalid) {String} message "Author parameter is invalid. An author should be a non-empty string."
- * @apiError (400: Rating Parameter Invalid) {String} message "Rating parameter is invalid. A rating should be a number between 0 and 5. Additionally, the minimum rating should be less than or equal to the maximum rating."
- * @apiError (400: Year Parameter Invalid) {String} message "Year parameter is invalid. A year should be a number between 1600 and 3000. Additionally, the minimum year should be less than or equal to the maximum year."
- * @apiError (404: Book not found) {String} message "No books found that meet the search criteria. Try again with a different search criteria."
- *
- */
-bookRouter.delete('/', (request, response) => {
-    response.send('Hello, World!');
-});
+bookRouter.delete('/isbn',
+    (request: Request, response: Response, next: NextFunction) => {
+        if(request.query.isbn === undefined) {
+            return response.status(400).send({
+                message: "Missing 'isbn' query paremeter."
+            });
+        }
+        if (
+            !validationFunctions.isNumberProvided(request.query.isbn) ||
+            !validationFunctions.validateISBN(request.query.isbn as unknown as number)
+        ) {
+            return response.status(400).send({
+                message: 'ISBN not valid. ISBN should be a positive 13 or 10 digit number.'
+            }); 
+        }
+        return next();
+    },
+    async (request: Request, response: Response) => {
+        const theQuery = selectBookInfo + ' WHERE book_isbn = $1 LIMIT 1;';
+        let values = [request.query.isbn];
+
+        let book = {}
+        await pool.query(theQuery, values)
+            .then((result) => {
+                if(result.rows.length === 0) {
+                    return response.status(404).send({
+                        message: 'Book not found'
+                    });
+                }
+                book = result.rows[0];
+            })
+            .catch((error) => {
+                //log the error
+                console.error('DB Query error on GET /isbn');
+                console.error(error);
+                return response.status(500).send({
+                    message: 'server error - contact support',
+                });
+            });
+
+        // if the response has already been sent, don't send it again
+        if(response.headersSent) return;
+
+        const theAuthorQuery = `SELECT author_name FROM
+                                BOOK_MAP LEFT JOIN AUTHORS ON author_id = id
+                                WHERE book_isbn=$1`
+        pool.query(theAuthorQuery, values)
+            .then((result) => {
+                book['authors'] = result.rows.map((row: { author_name: string; }) => row.author_name);
+                return response.send(book);
+            })
+            .catch((error) => {
+                //log the error
+                console.error('DB Query error on GET /isbn');
+                console.error(error);
+                return response.status(500).send({
+                    message: 'server error - contact support',
+                });
+            });
+    });
 
 export { bookRouter };
 
