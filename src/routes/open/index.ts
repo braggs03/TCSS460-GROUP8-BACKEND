0f3d0d5f1e7161f18a8a678bf0bd82fb5dd50779
Brandon <brandonragghianti@gmail.com>
2024-11-05 21:10:02 -0800

    Fixed compilation error.

--- src/routes/open/index.ts
+++ src/routes/open/index.ts
@@ -1,11 +1,9 @@
 import express, { Router } from 'express';
 
-import { messageRouter } from './message';
 import { bookRouter } from './book';
 
 const openRoutes: Router = express.Router();
 
-openRoutes.use('/message', messageRouter);
 openRoutes.use('/book/', bookRouter)
 
 export { openRoutes };
