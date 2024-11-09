c1701cb043fa2310fceb530603ced7f1c6bd540f
Jimothy <jsimp33@uw.edu>
2024-11-08 16:06:59 -0800

    Added bookroute to index.ts

--- src/routes/closed/index.ts
+++ src/routes/closed/index.ts
@@ -2,9 +2,11 @@ import express, { Router } from 'express';
 
 import { checkToken } from '../../core/middleware';
 import { tokenTestRouter } from './tokenTest';
+import { bookRouter } from './book';
 
 const closedRoutes: Router = express.Router();
 
 closedRoutes.use('/jwt_test', checkToken, tokenTestRouter);
+closedRoutes.use('/book/', bookRouter)
 
 export { closedRoutes };
